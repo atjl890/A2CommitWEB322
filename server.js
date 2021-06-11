d099685ca9b5b68303f5592d38feefd3ecf87eb1
Amanda <alee196@myseneca.ca>
2021-06-11 11:46:58 -0400

    commit6

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
  * Student number: 101630200
  * WEB322 Section NCC
  */
-
+var HTTP_PORT = process.env.PORT || 8080;
 const express = require('express')
 const app = express()
 const path = require('path')
-const port = 8080
+
 
 app.use(express.static(path.join(__dirname, "/images")));
 app.use(express.static(path.join(__dirname, "/css")));
