7f7c4e0828261c6307345e60f4e4b3449f9536f7
Amanda <alee196@myseneca.ca>
2021-06-29 15:03:36 -0400

    addMiddleware

--- server.js
+++ server.js
@@ -1,28 +1,25 @@
-/**** Amanda Lee
- * Student number: 101630200
- * WEB322 Section NCC
- */
+
 var HTTP_PORT = process.env.PORT || 8080;
-const express = require('express')
-const app = express()
-const path = require('path')
+const express = require("express");
 const exphbs = require('express-handlebars');
-
-
-
-app.use(express.static(path.join(__dirname, "/images")));
-app.use(express.static(path.join(__dirname, "/css")));
-app.engine('.hbs', exphbs({ extname: '.hbs' }));
+const app = express();
+app.engine(".hbs", exphbs({ extname: ".hbs" }));
 app.set('view engine', '.hbs');
 
-
+app.use(express.static(__dirname + "/images"));
 /*
 * Default route for the web app
 */
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "home.html"));
-  });
-
+  let home_info =
+  {      
+    welcome: "Welcome"
+   }; 
+  res.render("home", {
+  home_info : home_info,
+  layout: false
+});
+});
 
 
 app.get("/account", (req, res) =>{
@@ -36,6 +33,20 @@ app.get("/CWH", (req, res) =>{
   app.get("/signIn", (req, res) =>{
     res.sendFile(path.join(__dirname, "/signIn.html"))
   });
+app.post('/signInData', (req,res)=> {
+let emailErr;
+let passwordErr;
+  req.body = data;
 
+  if(data.inputEmail3.length ===0)
+  {
+emailErr = "Enter a valid email address"
+  }
+  if(data.inputPassword3.length ===0)
+  {
+passwordErr = "Enter a valid password"
+  }
+
+})
 
 app.listen(HTTP_PORT);
